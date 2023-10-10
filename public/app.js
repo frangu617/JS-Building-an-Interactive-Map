499b915a0f4cd4ca899c857950f1f3b2ae3b9139
frangu617 <142462584+frangu617@users.noreply.github.com>
2023-10-09 19:24:00 -0700

    mostly done, just tweak it for practice

--- public/app.js
+++ public/app.js
@@ -3,12 +3,15 @@
 
 
 // Create map
-
+const myMap = L.map('map').setView([48.868672, 2.342130], 12);
 
 
 
 // Add openstreetmap tiles
-
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(myMap);
 
 
 //Note: Replace 'https://.tile.openstreetmap.org///.png' with 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' in this copy/paste part in the Canvas activity instructions
@@ -16,15 +19,36 @@
 
 // Create and add a geolocation marker
 
-
-
-
-// Draw the 2nd arrondissement
+const marker = L.marker([48.87007, 2.346453])
+marker.addTo(myMap).bindPopup('<p1><b>Hello general Kenobi!</b></p1>').openPopup()
 
 
 
 
+// Draw the 2nd arrondissement  
+let polygon = L.polygon([                                   
+[48.863368120198004, 2.3509079846928516],
+[48.86933262048345, 2.3542531602919805],
+[48.87199261164275, 2.3400569901592183],
+[48.86993336274516, 2.3280142476578813],
+[48.86834104280146, 2.330308418109664],
+]).addTo(myMap).bindPopup('2nd arrondissement')
+
+
+let redIcon = L.icon({
+    iconUrl: '/assets/red-pin.png',
+    iconSize: [38, 38],
+   iconAnchor: [19, 38],
+})
 // Metro station markers
+const rS = L.marker([48.866200610611926, 2.352236247419453], {icon: redIcon}).bindPopup('Réaumur-Sébastopol')
+const sSD = L.marker([48.869531786321566, 2.3528590208055196], {icon: redIcon}).bindPopup('Strasbourg-Saint-Denis')
+const sentier = L.marker([48.8673721067762, 2.347107922912739], {icon: redIcon}).bindPopup('Sentier')
+const bourse = L.marker([48.86868503971672, 2.3412285142058167], {icon: redIcon}).bindPopup('Bourse')
+const qS = L.marker([48.869560129483226, 2.3358638645569543], {icon: redIcon}).bindPopup('Quatre Septembre')
+const gB = L.marker([48.871282159004856, 2.3434818588892714], {icon: redIcon}).bindPopup('Grands Boulevards')
+
+const stations = L.layerGroup([rS, sSD, sentier, bourse, qS, gB]).addTo(myMap)
 
 
 
